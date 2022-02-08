3d560491806df14ca525577c3c7960d4d2fc75e6
mt8 <info@mt8.biz>
2022-02-08 17:56:18 +0900

    add try-catch to shortcode

--- includes/v2/Core.php
+++ includes/v2/Core.php
@@ -2,6 +2,7 @@
 
 namespace mt8\BaseItemList;
 
+use Exception;
 use mt8\BaseItemList\Admin\Admin;
 use mt8\BaseItemList\Admin\View;
 
@@ -25,45 +26,53 @@ class Core {
 
 	public function add_shortcode( $atts ) {
 
-		//setup parameter
-		extract( shortcode_atts( 
-			array(
-				'q'     => '*',
-				'order' => '',
-				'sort'  => 'desc',
-				'limit' => 10,
-				'cache' => 60,
-				'name'  => 'cache',
-			), $atts ) );
+		try {
+			//setup parameter
+			extract( shortcode_atts( 
+				array(
+					'q'     => '*',
+					'order' => '',
+					'sort'  => 'desc',
+					'limit' => 10,
+					'cache' => 60,
+					'name'  => 'cache',
+				), $atts ) );
 
-		// check parameter
-		if ( ! in_array( $order, array( 'list_order', 'modified' ) ) ) {
-			$order = '';
-		}
-		if ( ! in_array( $sort, array( 'asc', 'desc' ) ) ) {
-			$sort = 'desc';
-		}
-		if ( 0 >= intval( $limit ) || $limit > 100 ) {
-			$limit = 10;
-		}
-		if ( 0 >= intval( $cache ) ) {
-			$cache = 60;
-		}
-
-		//call API if no cache
-		$json = get_transient( 'base-item-list-' . md5( $name ) );
-		if ( ! $json ) {
-			$json = $this->request_api( compact( 'q', 'order', 'sort', 'limit' ) );
-			if ( is_null( $json ) ) {
-				return '';
+			// check parameter
+			if ( ! in_array( $order, array( 'list_order', 'modified' ) ) ) {
+				$order = '';
 			}
-			if ( $cache > 0 ) {
-				set_transient( 'base-item-list-' . md5( $name ), $json, $cache );
+			if ( ! in_array( $sort, array( 'asc', 'desc' ) ) ) {
+				$sort = 'desc';
 			}
-		}
+			if ( 0 >= intval( $limit ) || $limit > 100 ) {
+				$limit = 10;
+			}
+			if ( 0 >= intval( $cache ) ) {
+				$cache = 60;
+			}
 
-		//print items
-		return $this->item_list( $json->items );
+			//call API if no cache
+			$json = get_transient( 'base-item-list-' . md5( $name ) );
+			if ( ! $json ) {
+				$json = $this->request_api( compact( 'q', 'order', 'sort', 'limit' ) );
+				if ( is_null( $json ) ) {
+					return '';
+				}
+				if ( $cache > 0 ) {
+					set_transient( 'base-item-list-' . md5( $name ), $json, $cache );
+				}
+			}
+
+			//print items
+			return $this->item_list( $json->items );
+
+		} catch ( Exception $ex ) {
+			error_log( '==========BASE Item List API Error==========' );
+			error_log( 'エラー:' . $ex->getMessage() );
+			update_option( self::LAST_ERROR_OPTION_KEY, 'エラー:' . $ex->getMessage(), false );
+			return '';
+		}
 
 	}
 	
